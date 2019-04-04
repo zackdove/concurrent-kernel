3ebca8990268a2a49dbe9523f442a50e0f7192eb
Zackeray Dove <zd17646@my.bristol.ac.uk>
2019-04-04 12:34:17 +0000

    meow

--- kernel/hilevel.c
+++ kernel/hilevel.c
@@ -13,6 +13,7 @@ pcb_t pcb[ maxProcesses ];
 pcb_t* current = NULL;
 extern uint32_t tos_process_stack; //Extern declares but doesn't define / allocate mem
 extern uint32_t tos_console;
+extern void main_console();
 int process_size_stack_offset = 0x1000;
 
 
@@ -42,7 +43,7 @@ void dispatch( ctx_t* ctx, pcb_t* prev, pcb_t* next ) {
 }
 
 //Round robin
-void schedule( ctx_t* ctx ) {
+void round_robin_schedule( ctx_t* ctx ) {
   if     ( current->pid == pcb[ 0 ].pid ) {
     dispatch( ctx, &pcb[ 0 ], &pcb[ 1 ] );      // context switch P_3 -> P_4
 
@@ -113,78 +114,44 @@ uint32_t get_stack_address_for_process(pid_t pid){
 
 
 void hilevel_handler_rst( ctx_t* ctx              ) {
-  /* Initialise three PCBs, representing user processes stemming from execution
-  * of two user programs.  Note in each case that
-  *
-  * - the CPSR value of 0x50 means the processor is switched into USR mode,
-  *   with IRQ interrupts enabled, and
-  * - the PC and SP values matche the entry point and top of stack.
-  */
-
-//   memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );     // initialise 0-th PCB = P_3
-//   pcb[ 0 ].pid      = 0;
-//   pcb[ 0 ].status   = STATUS_CREATED;
-//   pcb[ 0 ].ctx.cpsr = 0x50;
-//   pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_P3 );
-//   pcb[ 0 ].ctx.sp   = ( uint32_t )( &tos_P3  );
-//   pcb[ 0 ].base_priority = 1;
-//   pcb[ 0 ].priority = 1;
-
-//   memset( &pcb[ 1 ], 0, sizeof( pcb_t ) );     // initialise 1-st PCB = P_4
-//   pcb[ 1 ].pid      = 1;
-//   pcb[ 1 ].status   = STATUS_CREATED;
-//   pcb[ 1 ].ctx.cpsr = 0x50;
-//   pcb[ 1 ].ctx.pc   = ( uint32_t )( &main_P4 );
-//   pcb[ 1 ].ctx.sp   = ( uint32_t )( &tos_P4  );
-//   pcb[ 1 ].base_priority = 4;
-//   pcb[ 1 ].priority = 4;
-
-//   memset( &pcb[ 2 ], 0, sizeof( pcb_t ) );     // initialise 1-st PCB = P_5
-//   pcb[ 2 ].pid      = 2;
-//   pcb[ 2 ].status   = STATUS_CREATED;
-//   pcb[ 2 ].ctx.cpsr = 0x50;
-//   pcb[ 2 ].ctx.pc   = ( uint32_t )( &main_P5 );
-//   pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
-//   pcb[ 2 ].base_priority = 7;
-//   pcb[ 2 ].priority = 7;
-  
-  memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );     // initialise console process
-  pcb[ 0 ].pid      = 2;
+ 
+	// Initliase console process
+  memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );
+  pcb[ 0 ].pid      = 0;
   pcb[ 0 ].status   = STATUS_CREATED;
   pcb[ 0 ].ctx.cpsr = 0x50;
-  pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_P5 );
+  pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_console );
   pcb[ 0 ].ctx.sp   = ( uint32_t )( &tos_process_stack  );
   pcb[ 0 ].base_priority = 7;
   pcb[ 0 ].priority = 7;
   
 
-  /* Once the PCBs are initialised, we arbitrarily select the one in the 0-th
-  * PCB to be executed: there is no need to preserve the execution context,
-  * since it is is invalid on reset (i.e., no process will previously have
-  * been executing).
-  */
-  for (int i = 3; i < maxProcesses; i ++){
+  //Initliase the remaining empty/blank pcbs
+  for (int i = 1; i < maxProcesses; i ++){
     memset( &pcb[ i ], 0, sizeof( pcb_t ) );     // initialise ith PCB = P_i
     pcb[ i ].pid      = i;
     pcb[ i ].status   = STATUS_TERMINATED;
     pcb[ i ].ctx.cpsr = 0x50;
-    pcb[ i ].ctx.pc   = ( uint32_t )( &main_P5 ); //WHATTHEHELLISTHIS
+    pcb[ i ].ctx.pc   = ( uint32_t )( &main_console ); //WHATTHEHELLISTHIS
     pcb[ i ].ctx.sp   = ( uint32_t )( get_stack_address_for_process(i)  );
     pcb[ i ].base_priority = 0;
     pcb[ i ].priority = 0;
   }
 
+	//Initalise timer
   TIMER0->Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
   TIMER0->Timer1Ctrl  = 0x00000002; // select 32-bit   timer
   TIMER0->Timer1Ctrl |= 0x00000040; // select periodic timer
   TIMER0->Timer1Ctrl |= 0x00000020; // enable          timer interrupt
   TIMER0->Timer1Ctrl |= 0x00000080; // enable          timer
 
+	//Initalise the GIC (interupt handler)
   GICC0->PMR          = 0x000000F0; // unmask all            interrupts
   GICD0->ISENABLER1  |= 0x00000010; // enable timer          interrupt
   GICC0->CTLR         = 0x00000001; // enable GIC interface
   GICD0->CTLR         = 0x00000001; // enable GIC distributor
 
+	//Dispatch/execute the console
   dispatch( ctx, NULL, &pcb[ 0 ] );
   int_enable_irq();
 
@@ -219,10 +186,12 @@ pcb_t* get_empty_pcb(){
 }
 
 void terminate_process(pcb_t* process, ctx_t* ctx){
-	
+	process->status = STATUS_TERMINATED;
+	//Since the current program is now terminated, we need to run another by calling the scheduler
+	priority_queue_schedule(ctx);
 }
 
-void write(ctx_t* ctx){
+void sys_write(ctx_t* ctx){
 	int   fd = ( int   )( ctx->gpr[ 0 ] );
       char*  x = ( char* )( ctx->gpr[ 1 ] );
       int    n = ( int   )( ctx->gpr[ 2 ] );
@@ -234,7 +203,7 @@ void write(ctx_t* ctx){
       ctx->gpr[ 0 ] = n;
 }
 
-void read(ctx){
+void sys_read(ctx_t* ctx){
 	int   fd = ( int   )( ctx->gpr[ 0 ] );
       char*  x = ( char* )( ctx->gpr[ 1 ] );
       int    n = ( int   )( ctx->gpr[ 2 ] );
@@ -246,7 +215,7 @@ void read(ctx){
       ctx->gpr[ 0 ] = n;
 }
 
-void fork(ctx){
+void sys_fork( ctx_t* ctx){
 	pcb_t* child = get_empty_pcb(); //add check for no empty???
 	memcpy( &child->ctx, ctx, sizeof( ctx_t ) ); //Copy current context into child
 	child->status = STATUS_READY;
@@ -255,15 +224,23 @@ void fork(ctx){
 	//Put stack pointer into the child stack space, taking into account how far 'in' the pointer is
 	child->ctx.sp = get_stack_address_for_process(child->pid) - get_stack_address_for_process(current->pid) + ctx->sp;
 	//Copy accross stack
-	memcpy((void*)(child->ctx.sp), (void*) (ctx->sp), stack_offset);
+	memcpy((void*)(child->ctx.sp), (void*) (ctx->sp), process_size_stack_offset);
 	//Return 0 to child
 	child->ctx.gpr[0] = 0; 
 	//Return child PID to parent
 	ctx->gpr[0] = child->pid;
 }
 
-void exit(ctx_t* ctx){
+void sys_exit(ctx_t* ctx){
 	terminate_process(current, ctx);
+}
+
+void sys_exec(ctx_t* ctx){
+	PL011_putc( UART0, 'E', true );
+	//Address of 'new' program is stored in gpr[0]
+	ctx->pc = ctx->gpr[0];
+	//Set stack pointer to start, incase sp is halfway through, might not be needed
+	//ctx->sp = get_stack_address_for_process(getCurrentIndex);
 }
 
 void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
@@ -275,26 +252,30 @@ void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
   * - write any return value back to preserved usr mode registers.
   */
   switch( id ) {
-    case 0x00 : { // 0x00 => yield()
-      schedule( ctx );
+    case 0x00 : { // 0x00 => YIELD()
+      priority_queue_schedule( ctx );
       break;
     }
-    case 0x01 : { // 0x01 => write( fd, x, n )
-      write(ctx);
+    case 0x01 : { // 0x01 => WRITE( fd, x, n )
+      sys_write(ctx);
       break;
     }
-    case 0x02 :{ // 0x02 => read( fd, x, n )
-      read(ctx);
+    case 0x02 :{ // 0x02 => READ( fd, x, n )
+      sys_read(ctx);
       break;
 	}
-    case 0x03:{ // 0x03 = fork(parent, child)
-		fork(ctx);
+    case 0x03:{ // 0x03 => FORK(parent, child)
+		sys_fork(ctx);
 		break;
     }
-	case 0x04:{ //0x04 = exit
-		exit(ctx);
+	case 0x04:{ //0x04 => EXIT(process)
+		sys_exit(ctx);
 		break;
 	}
+	  case 0x05:{ //0x05 => EXEC
+		  sys_exec(ctx);
+		  break;
+	  }
   default   : { // 0x?? => unknown/unsupported
     break;
   }
--- kernel/lolevel.s
+++ kernel/lolevel.s
@@ -21,21 +21,45 @@ lolevel_handler_rst: bl    int_init                @ initialise interrupt vector
                      msr   cpsr, #0xD3             @ enter SVC mode with IRQ and FIQ interrupts disabled
                      ldr   sp, =tos_svc            @ initialise SVC mode stack
 
+                     sub   sp, sp, #68             @ initialise dummy context
+
+                     mov   r0, sp                  @ set    high-level C function arg. = SP
                      bl    hilevel_handler_rst     @ invoke high-level C function
-                     b     .                       @ halt
+
+                     ldmia sp!, { r0, lr }         @ load     USR mode PC and CPSR
+                     msr   spsr, r0                @ move     USR mode        CPSR
+                     ldmia sp, { r0-r12, sp, lr }^ @ restore  USR mode registers
+                     add   sp, sp, #60             @ update   SVC mode SP
+                     movs  pc, lr                  @ return from interrupt
 
 lolevel_handler_irq: sub   lr, lr, #4              @ correct return address
-                     stmfd sp!, { r0-r3, ip, lr }  @ save    caller-save registers
+                     stmia sp, { r0-r12, sp, lr }^ @ preserve USR registers
+                     mrs   r0, spsr                @ move     USR        CPSR
+                     stmdb sp!, { r0, lr }         @ store    USR PC and CPSR
 
+                     mov   r0, sp                  @ set    high-level C function arg. = SP
                      bl    hilevel_handler_irq     @ invoke high-level C function
 
-                     ldmfd sp!, { r0-r3, ip, lr }  @ restore caller-save registers
+                     ldmia sp!, { r0, lr }         @ load     USR mode PC and CPSR
+                     msr   spsr, r0                @ move     USR mode        CPSR
+                     ldmfd sp, { r0-r12, sp, lr }^ @ restore caller-save registers
+                     add   sp, sp, #60             @ update   SVC mode SP
+
                      movs  pc, lr                  @ return from interrupt
 
 lolevel_handler_svc: sub   lr, lr, #0              @ correct return address
-                     stmfd sp!, { r0-r3, ip, lr }  @ save    caller-save registers
+                     sub   sp, sp, #60             @ update   SVC mode stack
+                     stmia sp, { r0-r12, sp, lr }^ @ preserve USR registers
+                     mrs   r0, spsr                @ move     USR        CPSR
+                     stmdb sp!, { r0, lr }         @ store    USR PC and CPSR
 
+                     mov   r0, sp                  @ set    high-level C function arg. = SP
+                     ldr   r1, [ lr, #-4 ]         @ load                     svc instruction
+                     bic   r1, r1, #0xFF000000     @ set    high-level C function arg. = svc immediate
                      bl    hilevel_handler_svc     @ invoke high-level C function
 
-                     ldmfd sp!, { r0-r3, ip, lr }  @ restore caller-save registers
-                     movs  pc, lr                  @ return from interrupt 
+                     ldmia sp!, { r0, lr }         @ load     USR mode PC and CPSR
+                     msr   spsr, r0                @ move     USR mode        CPSR
+                     ldmia sp, { r0-r12, sp, lr }^ @ restore  USR mode registers
+                     add   sp, sp, #60             @ update   SVC mode SP
+                     movs  pc, lr                  @ return from interrupt
